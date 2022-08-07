1772237dd38702348914f71245bf8b71ef3f592f
EWA CHWEDCZUK <echwedczuk0@gmail.com>
2022-08-07 14:08:33 +0200

    Updates

---
+++ precache-manifest.cb6a1a550f174976a586966b9f333118.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef2fb5c538de00e1605a",
+    "url": "/ReactWeatherApp/static/css/main.ca0705aa.chunk.css"
+  },
+  {
+    "revision": "ef2fb5c538de00e1605a",
+    "url": "/ReactWeatherApp/static/js/main.ef2fb5c5.chunk.js"
+  },
+  {
+    "revision": "83c02d7c3c54af2a7d19",
+    "url": "/ReactWeatherApp/static/js/1.83c02d7c.chunk.js"
+  },
+  {
+    "revision": "8ff246e946d46ed7eb05",
+    "url": "/ReactWeatherApp/static/js/runtime~main.8ff246e9.js"
+  },
+  {
+    "revision": "bb6bcd86135dfabff2ff8e1d6645989b",
+    "url": "/ReactWeatherApp/static/media/cloudySky.bb6bcd86.jpg"
+  },
+  {
+    "revision": "342e85713497b30b7241e2ff0bd9f2b6",
+    "url": "/ReactWeatherApp/static/media/rain.342e8571.jpg"
+  },
+  {
+    "revision": "72de55ff98b6708f0edaa048bbac11e9",
+    "url": "/ReactWeatherApp/static/media/nightCloudy.72de55ff.jpg"
+  },
+  {
+    "revision": "2cae6a2de520aad5a9de457354da2bcf",
+    "url": "/ReactWeatherApp/static/media/nightRain.2cae6a2d.jpg"
+  },
+  {
+    "revision": "30fe9866c9002abf55683dd2519a30d8",
+    "url": "/ReactWeatherApp/static/media/snow.30fe9866.jpg"
+  },
+  {
+    "revision": "f054f94ee92a36826bd16a3a8d19641e",
+    "url": "/ReactWeatherApp/static/media/nightSnow.f054f94e.jpg"
+  },
+  {
+    "revision": "33c4fcea43e83a7fd4435ad43c12770d",
+    "url": "/ReactWeatherApp/static/media/clearSky.33c4fcea.jpg"
+  },
+  {
+    "revision": "fc134501507dd1de1c741bc5833ee5a0",
+    "url": "/ReactWeatherApp/static/media/clearSky.fc134501.jpg"
+  },
+  {
+    "revision": "b7b897d31b6678878d436541996a5dd1",
+    "url": "/ReactWeatherApp/index.html"
+  }
+];
